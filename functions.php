981f7d1e8e038dbc94cd0e3f39f7ab5983ef9f2d
Jason Braun <jasonbraun@me.com>
2015-12-14 10:36:21 -0800

    Style Content Area

--- functions.php
+++ functions.php
@@ -16,18 +16,18 @@ add_theme_support( 'genesis-responsive-viewport' );
 //* Enqueue scripts and styles
 add_action( 'wp_enqueue_scripts', 'hueman_scripts_styles' );
 function hueman_scripts_styles() {
-    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), 'treehouse-hueman-genesis' );
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), 'treehouse-hueman-genesis' );
 }
 
 //* Add support for structural wraps
 add_theme_support( 'genesis-structural-wraps', array(
-    'header', 'nav', 'subnav', 'main', 'footer-widgets', 'footer',
+	'header', 'nav', 'subnav', 'main', 'footer-widgets', 'footer',
 ) );
 
 //* Rename menus
 add_theme_support( 'genesis-menus', array(
-    'primary' => __( 'Header Top Navigation Menu', 'treehouse-hueman-genesis' ),
-    'secondary' => __( 'Header Bottom Navigation Menu', 'treehouse-hueman-genesis' )
+	'primary' => __( 'Header Top Navigation Menu', 'treehouse-hueman-genesis' ),
+	'secondary' => __( 'Header Bottom Navigation Menu', 'treehouse-hueman-genesis' )
 ) );
 
 //* Relocate Primary (top) Navigation
@@ -46,6 +46,10 @@ add_action( 'genesis_before', 'genesis_header_markup_close', 15 );
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
 add_action( 'genesis_before', 'genesis_do_subnav', 14 );
 
+//* Move the Secondary Sidebar into the Content Sidebar Wrap area.
+remove_action( 'genesis_after_content_sidebar_wrap', 'genesis_get_sidebar_alt' );
+add_action( 'genesis_after_content', 'genesis_get_sidebar_alt' );
+
 //* Add new image sizes
 add_image_size( 'home-top', 780, 354, TRUE );
 add_image_size( 'home-middle', 375, 175, TRUE );
@@ -55,12 +59,12 @@ add_theme_support( 'genesis-footer-widgets', 3 );
 
 //* Register widget areas
 genesis_register_sidebar( array(
-    'id'          => 'home-top',
-    'name'        => __( 'Home Top', 'treehouse-hueman-genesis' ),
-    'description' => __( 'Widgets in this section will display in the top widget area on the homepage.', 'treehouse-hueman-genesis' ),
+	'id'          => 'home-top',
+	'name'        => __( 'Home Top', 'treehouse-hueman-genesis' ),
+	'description' => __( 'Widgets in this section will display in the top widget area on the homepage.', 'treehouse-hueman-genesis' ),
 ) );
 genesis_register_sidebar( array(
-    'id'          => 'home-bottom',
-    'name'        => __( 'Home Bottom', 'treehouse-hueman-genesis' ),
-    'description' => __( 'Widgets in this section will display in the bottom widget area on the homepage.', 'treehouse-hueman-genesis' ),
+	'id'          => 'home-bottom',
+	'name'        => __( 'Home Bottom', 'treehouse-hueman-genesis' ),
+	'description' => __( 'Widgets in this section will display in the bottom widget area on the homepage.', 'treehouse-hueman-genesis' ),
 ) );
